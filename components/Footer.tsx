06e5e8ad4d905cb4428186265ec1e46ed3b879c5
tjmughal <tjmughal95@gmail.com>
2026-07-08 12:46:43 +0500

    UPDATE: remove github api for fetching fork_count from footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,17 +7,6 @@ interface RepoStats {
 }
 
 const Footer = async () => {
-    const repoStats = await fetch(
-        'https://api.github.com/repos/tjmughal/portfolio',
-        {
-            next: {
-                revalidate: 60 * 60, // 1 hour
-            },
-        },
-    );
-
-    const { stargazers_count, forks_count } =
-        (await repoStats.json()) as RepoStats;
 
     return (
         <footer className="text-center pb-5" id="contact">
@@ -46,21 +35,6 @@ const Footer = async () => {
                             </span>
                         </div>*/}
                     </a>
-
-                    {/* Note: If you are not Tajmirul, use this copyright message instead */}
-                    {/* <a href='https://www.me.toinfinite.dev/' className="leading-none text-muted-foreground hover:underline hover:text-white">
-                        Design & built by Tajmirul Islam <br />
-                        Revised by YOUR NAME
-
-                        <div className="flex items-center justify-center gap-5 pt-1">
-                            <span className='flex items-center gap-2'>
-                                <Star size={14} /> {stargazers_count}
-                            </span>
-                            <span className='flex items-center gap-2'>
-                                <GitFork size={14} /> {forks_count}
-                            </span>
-                        </div>
-                    </a> */}
                 </div>
             </div>
         </footer>
